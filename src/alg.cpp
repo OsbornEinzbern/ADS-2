945d1f4c84bed81614e79fead24f2d92ea43d59e
Vio5 <160281229+OsbornEinzbern@users.noreply.github.com>
2024-03-11 21:29:19 +0300

    Upd v.1.2.2 b151.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,47 +3,47 @@
 #include "alg.h"
 
 
-double pown(double value, uint16_t n) { //возведение числа в целую степень.
-    double res = 1;
-    for (uint16_t i = 0; i < n; i++) {
-        res *= value;
-    }
-    return res;
+double pown(double value, uint16_t n) {
+  double res = 1;
+  for (uint16_t i = 0; i < n; i++) {
+    res *= value;
+  }
+  return res;
 }
 
-uint64_t fact(uint16_t n) { //вычисление факториала чила n.
-    uint64_t result = 1;
-    for (uint64_t i = n; i >= 1; i--) {
-        result *= i;
-    }
-    return result;
+uint64_t fact(uint16_t n) {
+  uint64_t result = 1;
+  for (uint64_t i = n; i >= 1; i--) {
+    result *= i;
+  }
+  return result;
 }
 
-double calcItem(double x, uint16_t n) { //вычисление члена ряда.
-    uint64_t res = pown(x, n) / fact(n); 
-    return res;
+double calcItem(double x, uint16_t n) {
+  uint64_t res = pown(x, n) / fact(n);
+  return res;
 }
 
-double expn(double x, uint16_t count) { //вычисление значения функции exp(x) с суммированием count - членов ряда.
-    uint64_t sum = 0;
-    for (uint16_t i = 0; i <= count; i++) {
-        sum += calcItem(x, i);
-    }
-    return sum;
+double expn(double x, uint16_t count) {
+  uint64_t sum = 0;
+  for (uint16_t i = 0; i <= count; i++) {
+    sum += calcItem(x, i);
+  }
+  return sum;
 }
 
-double sinn(double x, uint16_t count) { //вычисление значения функции sin(x) с суммированием count - членов ряда.
-    uint64_t sum = 0;
-    for (uint16_t i = 0; i <= count; i++) {
-        sum += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
-    }
-    return sum;
+double sinn(double x, uint16_t count) {
+  uint64_t sum = 0;
+  for (uint16_t i = 0; i <= count; i++) {
+    sum += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+  }
+  return sum;
 }
 
-double cosn(double x, uint16_t count) { //вычисление значения функции cos(x) с суммированием count - членов ряда
-    uint64_t sum = 0;
-    for (uint16_t i = 0; i <= count; i++) {
-        sum += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
-    }
-    return sum;
+double cosn(double x, uint16_t count) {
+  uint64_t sum = 0;
+  for (uint16_t i = 0; i <= count; i++) {
+    sum += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+  }
+  return sum;
 }
